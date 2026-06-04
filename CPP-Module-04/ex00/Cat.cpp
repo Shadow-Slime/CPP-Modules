cc1c4031a7261db88453733a6a43470d2a9acf69
Daniel Gabriel Kirnoze Damiba <danielddamiba@gmail.com>
2026-06-04 12:11:57 +0100

    CPP04 ex00 started

---
+++ CPP-Module-04/ex00/Cat.cpp
@@ -0,0 +1,29 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &other) : Animal(other.type)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	*this = other;
+}
+
+Cat &Cat::operator=(const Cat &other)
+{
+	if (this != &other)
+		this->type = other.type;
+	return *this;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow" << std::endl;
+}
---
+++ CPP-Module-04/ex00/Dog.cpp
@@ -0,0 +1,29 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog")
+{
+	std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other) : Animal(other.type)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	*this = other;
+}
+
+Dog &Dog::operator=(const Dog &other)
+{
+	if (this != &other)
+		this->type = other.type;
+	return *this;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+	std::cout << "Bark" << std::endl;
+}
