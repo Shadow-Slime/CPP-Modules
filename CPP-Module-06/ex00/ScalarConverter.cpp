83ce50f4606e522db2a0d7e1763099b6190ccb2f
Daniel Damiba <ddamiba@c1r11s6.42lisboa.com>
2026-08-17 20:06:29 +0100

    progress

--- CPP-Module-06/ex00/ScalarConverter.cpp
+++ CPP-Module-06/ex00/ScalarConverter.cpp
@@ -1,11 +1,29 @@
 #include "ScalarConverter.hpp"
 
 
-
+static void print_pseudo_literal(std::string ps_lit)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	if (ps_lit == "nan" || ps_lit == "-inf" || ps_lit == "+inf")
+	{
+		std::cout << "float: " << ps_lit + "f" << std::endl;
+		std::cout << "double: " << ps_lit << std::endl;
+	}
+	else
+	{
+		std::cout << "float: " << ps_lit << std::endl;
+		std::cout << "double: " << ps_lit.substr(0, ps_lit.size()-1) << std::endl;
+	}
+}
 
 void ScalarConverter::convert(std::string literal)
 {
-
+	if (literal == "nan" || literal == "nanf" || literal == "-inf" || literal == "+inf" || literal == "+inff" || literal == "-inff")
+		return (print_pseudo_literal(literal));
+	char c;
+	int i;
+	
 }
 
 
--- CPP-Module-06/ex00/ScalarConverter.hpp
+++ CPP-Module-06/ex00/ScalarConverter.hpp
@@ -2,14 +2,15 @@
 #define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <string>
 
 class ScalarConverter
 {
 	public:
 		static void convert(std::string literal);
 	private:
-		ScalarConverter(const ScalarConverter &other) = delete;
-		ScalarConverter &operator=(const ScalarConverter &other) = delete;
+		ScalarConverter(const ScalarConverter &other);
+		ScalarConverter &operator=(const ScalarConverter &other);
 		~ScalarConverter();
 };
 
